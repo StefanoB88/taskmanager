079298e1d9ce15daa3d3f041b4abde2cb8e64ea8
Stefano Brandi <stefano.brandi1@hotmail.it>
2025-03-11 21:40:35 +0100

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Stefano on 11.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
